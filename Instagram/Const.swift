67db2463a905c8b5c3086565a0521ece0077836b
ochiai <ochiai.k@trend-art.net>
2021-06-20 08:01:47 +0900

    Lesson 8カリキュラム完了

---
+++ Instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by 落合克彦 on 2021/06/19.
+//
+
+import Foundation
+
+struct Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+}
